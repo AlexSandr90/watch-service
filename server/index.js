dfcc8c8ca1826774bca2cb446f36a2bc78d09aa7
Alexandr Gaytsuk <gayrsuk@gmail.com>
2021-07-12 17:18:13 +0300

    create structure

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+const PORT = config.get('serverPort');
+const URL = config.get('dbUrl');
+
+const start = async () => {
+    try {
+        await mongoose.connect(URL);
+
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+    } catch (error) {
+        console.warn(error);
+    }
+};
+
+start()
